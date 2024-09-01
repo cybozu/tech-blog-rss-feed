b8335ae32400441ae7c0ae7a5edfbe53d9c201d5
Junko KIJIMA <j-kijima@cybozu.co.jp>
2024-09-01 14:38:53 +0900

    appended sound contents to feed-info-lidt

--- src/resources/feed-info-list.ts
+++ src/resources/feed-info-list.ts
@@ -39,7 +39,10 @@ export const FEED_INFO_LIST: FeedInfo[] = createFeedInfoList([
   ['サイボウズ コネクト支援チーム', 'https://note.com/cybozutech/m/mc0c086e78c4f/rss'],
   ['Kintone Engineering Blog', 'https://blog.kintone.io/feed'],
   ['Cybozu Vietnam Tech Sharing', 'https://tech.cybozu.vn/rss.xml'],
-  ['サイボウズ 流氷交差点', 'https://note.com/cybozutech/m/m9707f4c496e6/rss'],
+  ['サイボウズ 流氷自由帳', 'https://note.com/cybozutech/m/m9707f4c496e6/rss'],
+  ['サイボウズ フロントエンド通信', 'https://anchor.fm/s/ec10d0c8/podcast/rss'],
+  ['Cybozu Design Podcast', 'https://feed.podbean.com/cybozudesign/feed.xml'],
+  ['流氷交差点', 'https://feeds.soundcloud.com/users/soundcloud:users:970362685/sounds.rss'],
  ]);
 
 /**
